e4e7c91fe5ee714abfd3f43aee3d51569c6556fd
Ran Crump <rancrump18@gmail.com>
2019-08-28 23:55:12 -0500

    + SMoke and new assets

---
+++ TopDownShooterProject/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 startingLocation;
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = transform.position;
+        startingLocation = transform.position;
+    }
+    
+    public void Update()
+    {
+        transform.position = startingLocation;
+    }
+}
---
+++ TopDownShooterProject/Assets/Scripts/SmokeScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeScreen : MonoBehaviour
+{
+    [Tooltip("The amount of time the smoke screen will go for")]
+    public float timer;
+    public GameObject SmokeEmiter;
+    private ParticleSystem ps;
+    private float inGameTimer;
+    private Rigidbody rb;
+    public void Start()
+    {
+        ps = SmokeEmiter.GetComponent<ParticleSystem>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void LateUpdate()
+    {    
+        if (rb.velocity.magnitude < .1f && !ps.isPlaying)
+        {
+            SmokeEmiter.transform.position = transform.position;
+            SmokeEmiter.transform.rotation = Quaternion.identity;
+            ps.Play();                        
+        }
+
+        if (inGameTimer < timer)
+        {
+            inGameTimer += Time.deltaTime;
+        }
+
+        if (inGameTimer >= timer)
+        {
+            ps.Stop();
+            Destroy(gameObject, 5f);
+        }
+    }
+}
